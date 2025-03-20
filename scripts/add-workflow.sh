c0d20381d428e3cb8d2e09a97ffbc3b94f3d5955
Josh Johanning <joshjohanning@github.com>
2025-03-20 10:53:56 -0500

    updating script

--- scripts/add-workflow.sh
+++ scripts/add-workflow.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# get the current date and time
+DATE=$(date +"%Y-%m-%d %H:%M:%S")
+
+
 # Relative path to the file to be replaced
 FILE="./.github/workflows/ci.yml"
 
@@ -9,7 +13,8 @@ REPLACE_WITH_FILE="$1"
 if [ -f "$FILE" ]; then
   echo "File $FILE exists."
   cp $REPLACE_WITH_FILE $FILE
-  echo "#" >> $FILE
+  # get the current date
+  printf '\n\n_last updated: %s\n' "$DATE" >> "$FILE"
   echo "File $FILE updated."
 else
   echo "File $FILE does not exist. Creating from $REPLACE_WITH_FILE...."
@@ -26,7 +31,7 @@ REPLACE_WITH_FILE_COPILOT="$2"
 if [ -f "$FILE_COPILOT" ]; then
   echo "File $FILE_COPILOT exists."
   cp $REPLACE_WITH_FILE_COPILOT $FILE_COPILOT
-  echo "#" >> $FILE_COPILOT
+  printf '\n\n_last updated: %s\n' "$DATE" >> "$FILE_COPILOT"
   echo "File $FILE_COPILOT updated."
 else
   echo "File $FILE_COPILOT does not exist. Creating from $REPLACE_WITH_FILE_COPILOT...."
