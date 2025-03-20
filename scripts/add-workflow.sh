de8da8baeb5759de8ad3286e8d8a72697713188c
Josh Johanning <joshjohanning@github.com>
2025-03-19 20:19:15 -0500

    updating

--- scripts/add-workflow.sh
+++ scripts/add-workflow.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Relative path to the file to be replaced
-FILE="./.github/dependabot.yml"
+FILE="./.github/workflows/ci.yml"
 
 # Full path to the file to copy in
 REPLACE_WITH_FILE="$1"
@@ -9,8 +9,8 @@ REPLACE_WITH_FILE="$1"
 if [ -f "$FILE" ]; then
   echo "File $FILE exists."
 else
-  echo "File $FILE does not exist. Creating..."
-  mkdir -p ./.github
+  echo "File $FILE does not exist. Creating from $REPLACE_WITH_FILE...."
+  mkdir -p ./.github/workflows
   cp $REPLACE_WITH_FILE $FILE
   echo "File $FILE created."
 fi
