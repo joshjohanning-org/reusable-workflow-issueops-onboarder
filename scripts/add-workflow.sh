1724c6cd09879e0a98d9cd4b4792e2cf05162a45
Josh Johanning <joshjohanning@github.com>
2025-03-21 11:44:36 -0500

    adding in dependabot

--- scripts/add-workflow.sh
+++ scripts/add-workflow.sh
@@ -1,26 +1,36 @@
 #!/bin/bash
 
 # get the current date and time
+# only using this for testing purposes so I can stamp the file with the current time/date if the file already exists
 DATE=$(date +"%Y-%m-%d %H:%M:%S")
 
+
+########################################
+# Workflow
+########################################
+
 # Relative path to the file to be replaced
-FILE="./.github/workflows/ci.yml"
+FILE_WORKFLOW="./.github/workflows/ci.yml"
 
 # Full path to the file to copy in
-REPLACE_WITH_FILE="$1"
+REPLACE_WITH_FILE_WORKFLOW="$1"
 
-if [ -f "$FILE" ]; then
-  echo "File $FILE exists."
-  cp $REPLACE_WITH_FILE $FILE
+if [ -f "$FILE_WORKFLOW" ]; then
+  echo "File $FILE_WORKFLOW exists."
+  cp $REPLACE_WITH_FILE_WORKFLOW $FILE_WORKFLOW
   # get the current date
-  printf '\n\n# last updated: %s\n' "$DATE" >> "$FILE"
-  echo "File $FILE updated."
+  printf '\n\n# last updated: %s\n' "$DATE" >> "$FILE_WORKFLOW"
+  echo "File $FILE_WORKFLOW updated."
 else
-  echo "File $FILE does not exist. Creating from $REPLACE_WITH_FILE...."
+  echo "File $FILE_WORKFLOW does not exist. Creating from $REPLACE_WITH_FILE_WORKFLOW...."
   mkdir -p ./.github/workflows
-  cp $REPLACE_WITH_FILE $FILE
-  echo "File $FILE created."
+  cp $REPLACE_WITH_FILE_WORKFLOW $FILE_WORKFLOW
+  echo "File $FILE_WORKFLOW created."
 fi
+
+########################################
+# Copilot Instructions File
+########################################
 
 FILE_COPILOT="./.github/copilot-instructions.md"
 
@@ -37,4 +47,40 @@ else
   mkdir -p ./.github
   cp $REPLACE_WITH_FILE_COPILOT $FILE_COPILOT
   echo "File $FILE_COPILOT created."
+fi
+
+
+########################################
+# Dependabot File
+########################################
+
+ADD_DEPENDABOT=$3
+
+if [ "$ADD_DEPENDABOT" != "true" ]; then
+  echo "Skipping Dependabot file creation."
+  exit 0
+else
+  echo "Creating Dependabot file."
+  # Relative path to the file to be replaced
+  FILE_DEPENDABOT="./.github/dependabot.yml"
+
+  # Check if the file exists
+  if [ -f "$FILE_DEPENDABOT" ]; then
+    echo "File $FILE_DEPENDABOT exists."
+    # Check if "package-ecosystem: github-actions" exists in the file
+    if [[ $(yq e '[.updates[] | select(.package-ecosystem == "github-actions")] | length' $FILE_DEPENDABOT) -gt 0 ]]; then
+      echo '"package-ecosystem: github-actions" exists in the file.'
+    else
+      echo '"package-ecosystem: github-actions" does not exist in the file.'
+      yq e '.updates += [{"package-ecosystem": "github-actions", "directory": "/", "schedule": {"interval": "daily"}}]' -i $FILE_DEPENDABOT
+      echo '"package-ecosystem: github-actions" added to the file.'
+    fi
+  else
+    echo "File $FILE_DEPENDABOT does not exist. Creating..."
+    mkdir -p ./.github
+    # Create the file with initial content
+    yq e '.version = "2"' - | \
+    yq e '.updates = [{"package-ecosystem": "github-actions", "directory": "/", "schedule": {"interval": "daily"}}]' - > "$FILE_DEPENDABOT"
+    echo "File $FILE_DEPENDABOT created."
+  fi
 fi
